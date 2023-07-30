2e7f057df35550c4b2711cde4b5831379b5d84d8
cassio <cassiolinhares99@gmail.com>
2023-07-30 11:34:09 -0300

    add index-router

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,11 @@ const routes = Router(); // exe. express
 const userRouter = require("./users.routes"); // import user.routes.js
 const notesRouter = require("./notes.routes");
 const tagsRouter = require("./tags.routes");
+const sessionsRouter = require("./sessions.routes");
 
 routes.use("/users", userRouter);
 routes.use("/notes", notesRouter);
 routes.use("/tags", tagsRouter);
+routes.use("/sessions", sessionsRouter);
 
 module.exports = routes; //export file
