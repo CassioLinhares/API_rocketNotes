9e8f22f90336480d015e0148d4bea484aff3a03e
cassio <cassiolinhares99@gmail.com>
2023-07-30 11:52:19 -0300

    update método create

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -2,7 +2,7 @@ class SessionsController {
     async create(request, response){
         const {email, password} = request.body;
 
-        return response.json(email, password);
+        return response.json({email, password});
     }
 }
 
