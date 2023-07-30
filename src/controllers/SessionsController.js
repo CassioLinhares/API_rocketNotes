a70ce405866b27a7d37e94a323deb51252fab129
cassio <cassiolinhares99@gmail.com>
2023-07-30 19:13:38 -0300

    auth-email/senha - geração de token p/ c/ user

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,8 +1,32 @@
+const knex = require("../database/knex");
+const appError = require("../utils/appError");
+const {compare} = require("bcryptjs");
+const authConfig = require("../config/auth");
+const {sign} = require("jsonwebtoken");
+
 class SessionsController {
     async create(request, response){
         const {email, password} = request.body;
 
-        return response.json({email, password});
+        const user = await knex("users").where({email}).first();
+
+        if (!user) {
+            throw new appError("Incorrect email and/our password", 401);
+        }
+
+        const passwordMatched = await compare(password, user.password);
+
+        if (!passwordMatched) {
+            throw new appError("Incorrect email and/our password", 401);
+        }
+
+        const {secret, expiresIn} = authConfig.jwt;
+        const token = sign({}, secret, {
+            subject: String(user.id),
+            expiresIn
+        });
+
+        return response.json({user, token});
     }
 }
 
