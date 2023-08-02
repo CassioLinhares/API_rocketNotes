b3c3dad1b9ca8bce2fd6d20bcd4a5b46b66b3c76
cassio <cassiolinhares99@gmail.com>
2023-08-02 20:53:40 -0300

    update notes.routes - middleware

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
 const notesRouter = Router();
 
+const ensureAuthenticated = require("../middleware/ensureAuthenticated");
+
 const NotesController = require("../controllers/NotesController");
 const notesController = new NotesController();
+
+notesRouter.use(ensureAuthenticated); //apply middleware to all notesRouter methods
 
 notesRouter.get("/", notesController.index); //when to use QUERY don't need to insert /something
 notesRouter.post("/:user_id", notesController.create);
