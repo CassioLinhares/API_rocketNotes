1863fefc8304627e784f6c0c067dd0763e8efb04
cassio <cassiolinhares99@gmail.com>
2023-08-02 20:57:33 -0300

    update tagsRouter - middleware

--- src/routes/tags.routes.js
+++ src/routes/tags.routes.js
@@ -1,9 +1,11 @@
 const {Router} = require("express");
 const tagsRouter = Router();
 
+const ensureAuthenticated = require("../middleware/ensureAuthenticated");
+
 const TagsController = require("../controllers/tagsController");
 const tagsController = new TagsController();
 
-tagsRouter.get("/:user_id", tagsController.index);
+tagsRouter.get("/", ensureAuthenticated, tagsController.index);
 
 module.exports = tagsRouter;
