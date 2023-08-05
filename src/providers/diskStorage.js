29d5ef874f4e0b7f3c6a65fbfc749a2bd07d03be
cassio <cassiolinhares99@gmail.com>
2023-08-04 23:26:29 -0300

    create method save and delete fileUpload

---
+++ src/providers/diskStorage.js
@@ -0,0 +1,27 @@
+const fs = require("fs"); //file management
+const path = require("path");
+const uploadConfig = require("../config/upload");
+
+class DiskStorage{
+    async saveFile(file){
+        await fs.promises.rename(
+            path.resolve(uploadConfig.TMP_FOLDER, file),
+            path.resolve(uploadConfig.UPLOAD_FOLDER, file)
+        );
+        return file;
+    }
+
+    async deleteFile(file){
+        const filePath = path.resolve(uploadConfig.UPLOAD_FOLDER, file);
+
+        try {
+            await fs.promises.stat(filePath);//check if the file exists
+        } catch {
+            return;
+        }
+        await fs.promises.unlink(filePath);//delete
+        return file;
+    }
+}
+
+module.exports = DiskStorage;
